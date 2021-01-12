0c629411980c084b68aed9a7ae0aa97aea127795
Sergey030520 <Malipa1941@yandex.ru>
2021-01-12 14:27:53 +0300

    added the implementation of the car's digital speedometer

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <sstream>
+
+bool compare (float speed, float border_speed, float epsilon){
+    return ((speed <= border_speed + epsilon) && (speed >= border_speed - epsilon));
+}
 
 int main() {
-
+    float speed = 0.0f, delta = 0.01;
+    std::stringstream speedometer;
+    do{
+        std::cout << "Input delta speed: ";
+        std::cin >> speed;
+        if (compare(speed, 150, delta)) continue;
+        speedometer << "Speed: " << (speed * 10)/10 << "km/h";
+        std::cout << speedometer.str() << std::endl;
+        speedometer.str("");
+    } while (!compare(speed, 0, delta));
+    return 0;
 }
